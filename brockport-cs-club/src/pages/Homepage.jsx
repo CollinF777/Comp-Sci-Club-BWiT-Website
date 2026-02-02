ac9662bfc1fc4f8357b840ffc48834da761aa3de
Collin F <thedrunkninja98@gmail.com>
2026-02-02 11:29:54 -0500

    feat: Made Homepage.jsx

---
+++ brockport-cs-club/src/pages/Homepage.jsx
@@ -0,0 +1,150 @@
+import { MessageSquare, Instagram } from "lucide-react";
+
+function HomePage() {
+    return (
+        <main className="main-content">
+            <div className="two-column-grid">
+                <div className="left-column">
+                    <h2 className="column-title cs-title">Computing Sciences Club</h2>
+
+                    <section className="card cs-card">
+                        <h3 className="card-title">Who are we?</h3>
+                        { /* Like 90% of this text-wise is taken from Naya's presentation she made so judge her writing not mine */ } 
+                        { /* She also really likes exclamation points, although apparently that was also Ryan */ }
+                        <p className="card-text">
+                            We are a group of Computer Science students who aim to bring likeminded 
+                            individuals together to have fun and network in a friendly environment!!!
+                        </p>
+                    </section>
+
+                    <section className="card cs-card">
+                        <h3 className="card-title">What do we do?</h3>
+                        <p className="card-text">
+                            We do a variety of activities to destress from the Computer Science student workload, such as:
+                        </p>
+                        <ul className="activity-list">
+                            <li>Icebreaker activities</li>
+                            <li>Board Games</li>
+                            <li>Skill-building workshops</li>
+                            <li>Code Showcases</li>
+                            <li>Video Game sessions</li>
+                            <li>Movie/TV nights</li>
+                            <li>Pizza parties</li>
+                            <li>Alumni/Guest Talks</li>
+                            <li>...And more!</li>
+                        </ul>
+                    </section>
+
+                    <div className="event-highlights">
+                        <div className="event-box gold-event">
+                            <img src="/Images/CSClubFall25.jpg" alt="A picture of the members of the comp sci club" />
+                            <h4>First pizza party of last semester!</h4>
+                        </div>
+                        <div className="event-box green-event">
+                            <img src="/Images/CSClubGames.jpg" alt="A picture of the members of the comp sci club playing games" />
+                            <h4>Games, Competitions, Prizes...Pizza!!!!!!!!!</h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="right-column">
+                    <h2 className="column-title bwit-title">Brockport Women in Tech</h2>
+
+                    <section className="card bwit-card">
+                        <h3 className="card-title">Who are we?</h3>
+                        <p className="card-text">
+                            Celebrating and uniting the women of the Brockport Computer Science department, 
+                            our goal is to provide a safe and welcoming space and open ear for women in a field 
+                            where it can be difficult to feel seen or heard.
+                        </p>
+                    </section>
+
+                    <section className="card bwit-card">
+                        <h3 className="card-title">What do we do?</h3>
+                        <ul className="activity-list">
+                            <li>Arts & Crafts
+                                <ul className="sub-list">
+                                    <li>Coloring Books</li>
+                                    <li>Bracelet Making</li>
+                                    <li>Making Nametags</li>
+                                </ul>
+                            </li>
+                            <li>Movie Nights</li>
+                            <li>Skill-building Workshops</li>
+                            <li>Talk Sessions</li>
+                            <li>Study Groups</li>
+                            <li>Pizza parties</li>
+                            <li>Alumni/Guest Talks</li>
+                            <li>...And more!</li>
+                        </ul>
+                    </section>
+
+                    <div className="event-highlights">
+                        <div className="event-box gold-event">
+                            <img src="/Images/BWiTMembers.jpg" alt="A picture of the members of BWiT" />
+                            <h4>First meeting!</h4>
+                        </div>
+                        <div className="event-box green-event">
+                            <img src="/Images/BWiTColoring.jpg" alt="A picture of the members of BWiT coloring" />
+                            <h4>Pizza & Bingo!</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="alumni-banner">
+                <div className="alumni-banner-images">
+                    <img src="/Images/BWiTSpeaker.png" alt="A picture of a BWiT guest speaker event" />
+                    <img src="/Images/CSClubSpeaker.png" alt="A picture of a CS Club guest speaker event" />
+                </div>
+                <h3>Club Sponsored Speakers!</h3>
+            </div>
+
+            <section className="location-section">
+                <h2 className="location-title">But where?!?!?!?!</h2>
+                <div className="location-building">
+                    <div className="location-details">
+                        <p className="location-building">A.W. Brown Building (Academic Success Center)</p>
+                        <p className="location-room">Comtec Student Lounge</p>
+                        <p className="location-welcome">Everyone is welcome!</p>
+                    </div>
+                    <div className="location-image">
+                        <img src="/Images/ASC.jpg" alt="A picture of the Academic Success Center"/>
+                    </div>
+                </div>
+            </section>
+
+            <div className="social-grid">
+                <section className="card cs-card">
+                    <h3 className="card-title">Computing Sciences Club</h3>
+                    <div className="social-links-plain">
+                        <a href="https://discord.com/invite/6hUCrXdXvd" target="_blank" rel="noopener noreferrer" className="social-link-plain">
+                            <MessageSquare className="social-icon" />
+                            <span>Discord!</span>
+                        </a>
+                        <a href="https://instagram.com/bport.csc" target="_blank" rel="noopener noreferrer" className="social-link-plain">
+                            <Instagram className="social-icon" />
+                            <span>Instagram: bport.csc</span>
+                        </a>
+                    </div>
+                </section>
+
+                <section className="card bwit-card">
+                    <h3 className="card-title">BWiT</h3>
+                    <div className="social-links-plain">
+                        <a href="https://discord.gg/9tjUeAS5X" target="_blank" rel="noopener noreferrer" className="social-link-plain">
+                            <MessageSquare className="social-icon" />
+                            <span>Discord!</span>
+                        </a>
+                        <a href="https://instagram.com/brockportwomenintechnology" target="_blank" rel="noopener noreferrer" className="social-link-plain">
+                            <Instagram className="social-icon" />
+                            <span>Instagram: brockportwomenintechnology</span>
+                        </a>
+                    </div>
+                </section>
+            </div>
+        </main>
+    );
+}
+
+export default HomePage;
