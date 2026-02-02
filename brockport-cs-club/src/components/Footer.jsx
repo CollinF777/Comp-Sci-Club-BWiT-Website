a7e3cec8f0dc65bc62d4cd752bba0247c9dfb695
Collin F <thedrunkninja98@gmail.com>
2026-02-02 11:31:04 -0500

    feat: Made Footer.jsx

---
+++ brockport-cs-club/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer">
+             <p>Join us in building a vibrant Computer Science community at Brockport!</p>
+        </footer>
+    );
+}
+
+export default Footer;
