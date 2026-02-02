748b5c3daf2480aafd0e992ee3b2fa50abba33c5
Collin F <thedrunkninja98@gmail.com>
2026-02-02 11:29:39 -0500

    feat: Added React Router

--- brockport-cs-club/src/App.jsx
+++ brockport-cs-club/src/App.jsx
@@ -1,8 +1,27 @@
-import ClubMainPage from "./pages/ClubMainPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import CSClubPage from "./pages/CSClubPage";
+import BWiTPage from "./pages/BWiTPage";
+import ContactPage from "./pages/ContactPage";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-    return <ClubMainPage />;
+    return (
+        <Router>
+            <div className="site-container">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/cs-club" element={<CSClubPage />} />
+                    <Route path="/bwit" element={<BWiTPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
